e146ba029d8cb2459fb4e3792bae9aa047e985c9
acardo02 <00037820@uca.edu.sv>
2023-06-07 21:15:14 -0600

    feat: adding datbinding to the fragment

--- app/src/main/java/com/mcmp2023/s/ProductDescriptionFragment.kt
+++ app/src/main/java/com/mcmp2023/s/ProductDescriptionFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mcmp2023.s.databinding.FragmentProductDescriptionBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,12 +14,14 @@ import android.view.ViewGroup
  */
 class ProductDescriptionFragment : Fragment() {
 
+    private lateinit var binding: FragmentProductDescriptionBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_description, container, false)
+        binding = FragmentProductDescriptionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
