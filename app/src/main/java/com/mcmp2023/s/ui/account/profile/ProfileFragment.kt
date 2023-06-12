0c2addf9ac7013f4b2191ac7ba5e6fc085f92d88
acardo02 <00037820@uca.edu.sv>
2023-06-12 10:14:53 -0600

    feat: Adding settings screen

--- app/src/main/java/com/mcmp2023/s/ui/account/profile/ProfileFragment.kt
+++ app/src/main/java/com/mcmp2023/s/ui/account/profile/ProfileFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mcmp2023.s.R
+import com.mcmp2023.s.databinding.FragmentProfileBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,12 +16,22 @@ import com.mcmp2023.s.R
  */
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.settingsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
+        }
     }
 
 }
