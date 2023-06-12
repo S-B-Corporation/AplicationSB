b46c6b979476f7c7d8a30cb43f3439ff1cdb0032
davidperlera <misaelramirez123a@gmail.com>
2023-06-12 15:33:34 -0600

    feat: Add login Response

---
+++ app/src/main/java/com/mcmp2023/s/network/dto/login/LoginResponse.kt
@@ -0,0 +1,8 @@
+package com.mcmp2023.s.network.dto.login
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @SerializedName("message") val message: String,
+    @SerializedName("token") val token: String
+)
