0f54bbe7a88523055f7a837b8433b6c5a3ee04f5
davidperlera <misaelramirez123a@gmail.com>
2023-06-19 03:24:00 -0600

    feat: add RestorePasswordRepository with forgotPassword fun

--- app/src/main/java/com/mcmp2023/s/repositoires/credentialsrepo/RestorePasswordRepository.kt
+++ app/src/main/java/com/mcmp2023/s/repositoires/credentialsrepo/RestorePasswordRepository.kt
@@ -1,4 +1,26 @@
 package com.mcmp2023.s.repositoires.credentialsrepo
 
-class RestorePasswordRepository {
+import com.mcmp2023.s.network.ApiResponse
+import com.mcmp2023.s.network.dto.forgotPassword.ForgotPasswordRequest
+import com.mcmp2023.s.network.dto.forgotPassword.ForgotPasswordResponse
+import com.mcmp2023.s.network.service.RestorePasswordService
+import retrofit2.HttpException
+import java.io.IOException
+
+class RestorePasswordRepository(private val api: RestorePasswordService) {
+    suspend fun forgotPassword(email: String): ApiResponse<String> {
+        try {
+            val response: ForgotPasswordResponse
+            = api.forgotPassword(ForgotPasswordRequest(email))
+
+            return ApiResponse.Success(response.message)
+        } catch (e: HttpException) {
+            if (e.code() == 404){
+                return ApiResponse.ErrorWithMessage("Email not found")
+            }
+            return ApiResponse.Error(e)
+        }catch (e: IOException){
+            return ApiResponse.Error(e)
+        }
+    }
 }
