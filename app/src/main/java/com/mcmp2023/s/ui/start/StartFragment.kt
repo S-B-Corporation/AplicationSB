d2319ae3b94aaa2190af9013744fe1a4d608ce08
Carlosuca <00202118@uca.edu.sv>
2023-06-29 23:57:36 -0600

    add: start to app

---
+++ app/src/main/java/com/mcmp2023/s/ui/start/StartFragment.kt
@@ -0,0 +1,38 @@
+package com.mcmp2023.s.ui.start
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.mcmp2023.s.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_start_to_login).setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_createAccount)
+        }
+    }
+
+}
